4c65e2ec069bd5d6fb3dcb62b2c78c94f87fbfd4
gaston pedraza <gp.ju.dev@gmail.com>
2022-07-14 13:37:39 -0300

    refactor

---
+++ js/controls.js
@@ -0,0 +1,47 @@
+function createControls(scene) {
+    //set up input map
+    var inputMap = {};
+    scene.actionManager = new BABYLON.ActionManager(scene);
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function(evt) {
+      inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+
+    }));
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function(evt) {
+      inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+
+    }));
+
+    window.addEventListener('keyup', function (e) {
+      if (e.key === 'w' || e.key === 'ArrowUp') {
+        collider.animationGroups[3].stop()
+      } else if (e.key === 's' || e.key === 'ArrowDown') {
+        collider.animationGroups[0].stop()
+      }
+    })
+
+    scene.onBeforeRenderObservable.add(() => {
+      if (collider) {
+        if (inputMap["w"] || inputMap["ArrowDown"]) {
+          collider.animationGroups[3].start()
+          collider.moveWithCollisions(collider.frontVector.multiplyByFloats(collider.speed, collider.speed, collider.speed));
+          socket.emit('move', { key: "w", id: id, position: collider.position, rotation: collider.rotation})
+        }
+        if (inputMap["s"] || inputMap["ArrowUp"]) {
+          collider.animationGroups[0].start()
+          collider.moveWithCollisions(collider.frontVector.multiplyByFloats(-collider.speed / 2, -collider.speed / 2, -collider.speed / 2));
+          socket.emit('move', { key: "s", id: id, position: collider.position, rotation: collider.rotation})
+        }
+        if (inputMap["a"] || inputMap["ArrowLeft"]) {
+          collider.rotation.y -= .05;
+          collider.frontVector = new BABYLON.Vector3(Math.sin(collider.rotation.y), 0, Math.cos(collider.rotation.y));
+          socket.emit('move', { id: id, position: collider.position, rotation: collider.rotation})
+        }
+        if (inputMap["d"] || inputMap["ArrowRight"]) {
+          collider.rotation.y += .05;
+          
+          collider.frontVector = new BABYLON.Vector3(Math.sin(collider.rotation.y), 0, Math.cos(collider.rotation.y));
+          socket.emit('move', { id: id, position: collider.position, rotation: collider.rotation})
+        }
+      }
+    })
+}
---
+++ js/player.js
@@ -0,0 +1,23 @@
+async function loadPlayer() {
+    let { meshes: newMeshes, animationGroups } = await BABYLON.SceneLoader.ImportMeshAsync("", "./scenes/", "b.glb", scene);
+
+    collider = BABYLON.MeshBuilder.CreateCapsule("collider", { height: 4, radius: 1.5}, scene);
+    collider.name = id;
+    collider.animationGroups = animationGroups;
+    collider.animationGroups[0].start();
+    collider.animationGroups[0].stop();
+    collider.animationGroups[1].play(true);
+    collider.position.y = 0.75;
+    collider.speed = 0.2;
+    collider.frontVector = new BABYLON.Vector3(Math.sin(collider.rotation.y), 0, Math.cos(collider.rotation.y));
+    collider.checkCollisions = true;
+    collider.rotationQuaternion = undefined;
+    collider.isVisible = false;
+
+    var playerCollider = newMeshes[1];
+    playerCollider.setParent(collider);
+    playerCollider.position = new BABYLON.Vector3(0, 0, 0);
+    return;
+}
+
+        
